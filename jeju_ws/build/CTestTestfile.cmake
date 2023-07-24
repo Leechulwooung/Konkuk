96ededb807c871b2de6def6f062d5a2474d489bd
Leechulwooung <cwl1223@naver.com>
2023-07-24 15:26:20 +0900

    국제대학생제2회EV자율주행경진대회

---
+++ jeju_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/amap/jeju_ws/src
+# Build directory: /home/amap/jeju_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("opencv_line_detection")
+subdirs("m_car_new")
---
+++ jeju_ws/build/opencv_line_detection/CMakeFiles/ros_opencv_line_detection_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ros_opencv_line_detection_node.dir/src/opencv_line_detect.cpp.o"
+  "/home/amap/jeju_ws/devel/lib/opencv_line_detection/ros_opencv_line_detection_node.pdb"
+  "/home/amap/jeju_ws/devel/lib/opencv_line_detection/ros_opencv_line_detection_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ros_opencv_line_detection_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
